c8262a0ac1bccf802ba21b12f1f3909c69032ce6
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-05 14:14:43 +0300

    redirect

--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
 
 MONTHLY_CHALLENGES = {
-    'january': 'Eat no meat for the entire month!',
-    'february': 'Walk for at least 20 mins every day!',
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
 }
 
 
@@ -18,4 +28,9 @@ def monthly_challenge(request, month):
 
 
 def monthly_challenge_by_month_number(request, month: int):
-    return HttpResponse(month)
+    months = list(MONTHLY_CHALLENGES.keys())
+    try:
+        redirect_month = months[month-1]
+        return HttpResponseRedirect(f'/challenges/{redirect_month}')
+    except IndexError:
+        return HttpResponseNotFound('This month is not supported!')
